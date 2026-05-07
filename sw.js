65f8a0ca419f0b4f1125a44891ac6049e1925d6f
freegamefire03-boop <freegamefire03@gmail.com>
2026-05-07 22:26:52 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -4,6 +4,7 @@ const ASSETS = [
   '.',
   'index.html',
   'manifest.json',
+  'words.json',
   'icon-192.png',
   'icon-512.png',
   'https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Nunito:wght@400;600;700;800;900&display=swap'
